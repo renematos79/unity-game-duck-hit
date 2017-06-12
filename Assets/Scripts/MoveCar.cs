672eea115ab2748c62172413a6b42a04be281a4c
renato matos <renato.matos79@gmail.com>
2017-06-12 03:05:10 -0300

    Sistema de particulas

--- Assets/Scripts/MoveCar.cs
+++ Assets/Scripts/MoveCar.cs
@@ -8,12 +8,21 @@ public class MoveCar : MonoBehaviour {
 	public MoveCarDirection CarDirection = MoveCarDirection.Up;
 	public float Speed = 2.5f;
 	public float OldSpeed = 2.5f;
+	public GameObject SmokeParticle;
 
 	// Use this for initialization
 	void Start () {
-		
+		if (this.SmokeParticle != null) {
+			InvokeRepeating ("CreateSmokeParticle", 2.0f, 2.0f);
+		}
 	}
-	
+
+	private void CreateSmokeParticle(){
+		var smoke = Instantiate (SmokeParticle);
+		smoke.gameObject.transform.parent = this.transform.parent;
+		smoke.transform.position = this.transform.position;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (this.CarDirection == MoveCarDirection.None) {
---
+++ Assets/Scripts/RemoveObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("RemoveGameObject", 1.5f);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void RemoveGameObject(){
+		DestroyObject (this.gameObject);
+	}
+}
